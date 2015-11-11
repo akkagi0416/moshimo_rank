7ab0cbe4bdf0c0abe482f49338fed45aea112cd5
akkagi0416 <akkagi0416@gmail.com>
2015-11-11 20:55:37 +0900

    add google charts @ article show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find_by(article_id: params[:id])
+    @category = @article.category_code[0..2]    # category取得 '01'など
   end
 end
