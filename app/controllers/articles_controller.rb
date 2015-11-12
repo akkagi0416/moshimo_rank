0a6826f04e796bbc989f600650f667cf178ed450
akkagi0416 <akkagi0416@gmail.com>
2015-11-12 15:15:25 +0900

    i can use db data @ google charts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def show
     @article = Article.find_by(article_id: params[:id])
-    @category = @article.category_code[0..2]    # category取得 '01'など
+    @rankings = @article.rankings
+    @category = @article.category_code[0..1]    # category取得 '01'など
   end
 end
